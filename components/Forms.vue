3accc68a9f94e53ac6e3fed60459dd9311a15496
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-09 21:58:29 +0100

    dfd

--- components/Forms.vue
+++ components/Forms.vue
@@ -1,47 +1,61 @@
 <template>
   <div class="forms">
-    <b-row class="my-5">
-      <b-col sm="6">
-        <b-form-input
-          v-model="text"
-          placeholder="Author first name"
-        ></b-form-input>
-        <b-form-input
-          v-model="text"
-          placeholder="Author last name"
-        ></b-form-input>
-        <b-form-input v-model="text" placeholder="Author ID"></b-form-input>
-        <b-button variant="primary" type="submit" value="create author"
-          >Create Author</b-button
-        >
-      </b-col>
-    </b-row>
-    <b-row class="my-5">
-      <b-col sm="6">
-        <b-form-input v-model="text" placeholder="Genre"></b-form-input>
-        <b-form-input v-model="text" placeholder="Genre ID"></b-form-input>
+    <div id="author-form" @keyup="test">
+      <b-row class="my-5">
+        <b-col sm="6">
+          <b-form-input
+            v-model="text"
+            placeholder="Author first name"
+          ></b-form-input>
+          <b-form-input
+            v-model="text"
+            placeholder="Author last name"
+          ></b-form-input>
+          <b-form-input v-model="text" placeholder="Author ID"></b-form-input>
+          <b-button variant="primary" type="submit" value="create author"
+            >Create Author</b-button
+          >
+        </b-col>
+      </b-row>
+    </div>
+    <div id="genre-form">
+      <b-row class="my-5">
+        <b-col sm="6">
+          <b-form-input v-model="text" placeholder="Genre"></b-form-input>
+          <b-form-input v-model="text" placeholder="Genre ID"></b-form-input>
 
-        <b-button variant="primary" type="submit" value="create genre"
-          >Create Genre</b-button
-        >
-      </b-col>
-    </b-row>
-    <b-row class="my-5">
-      <b-col sm="6">
-        <b-form-input v-model="text" placeholder="Book title"></b-form-input>
-        <b-form-input v-model="text" placeholder="Release year"></b-form-input>
-        <!--       <b-form-select v-model="selected" :options="optionOne"></b-form-select> !-->
-        <b-form-select
-          v-model="selected"
-          :options="option.text"
-          v-for="option in options"
-          :key="option.text"
-        ></b-form-select>
-        <b-button variant="success" type="submit" value="create author"
-          >Create Book</b-button
-        >
-      </b-col>
-    </b-row>
+          <b-button variant="primary" type="submit" value="create genre"
+            >Create Genre</b-button
+          >
+        </b-col>
+      </b-row>
+    </div>
+    <div id="book-creator">
+      <b-row class="my-5">
+        <b-col sm="6">
+          <b-form-input v-model="text" placeholder="Book title"></b-form-input>
+          <b-form-input
+            v-model="text"
+            placeholder="Release year"
+          ></b-form-input>
+          <b-form-select
+            v-model="select.selected"
+            :options="select.optionOne"
+            v-for="option in select.optionOne"
+            :key="option"
+          ></b-form-select>
+          <b-form-select
+            v-model="select.selected"
+            :options="select.optionTwo"
+            v-for="option in select.optionTwo"
+            :key="option"
+          ></b-form-select>
+          <b-button variant="success" type="submit" value="create author"
+            >Create Book</b-button
+          >
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
@@ -49,13 +63,23 @@
 export default {
   data() {
     return {
-      selected: null,
-      // Nie wiem jak zrobić to dynamicznie więc na razie robię tak.
-      options: [
-        { value: null, text: "Author" },
-        { value: null, text: "Genre" },
-      ],
+      select: {
+        selected: null,
+        optionOne: [{ value: null, text: "Author" }],
+        optionTwo: [{ value: null, text: "Genre" }],
+      },
+      authors: [{ d }, {}, {}],
+      genres: [],
+      booklist: [],
+      AuthorObject: {},
+      GenreObject: {},
+      BookObject: {},
     };
+  },
+  methods: {
+    test: function (event) {
+      console.log(event.key);
+    },
   },
 };
 </script>
