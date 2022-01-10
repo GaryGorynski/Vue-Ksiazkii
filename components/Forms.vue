5b791e5e4843597ab2080252c0f56850204c6f8f
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-10 14:18:26 +0100

    create author/create genre

--- components/Forms.vue
+++ components/Forms.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="forms">
-    <div id="author-form" @keyup="test">
+    <div id="author-form">
       <b-row class="my-5">
         <b-col sm="6">
           <b-form-input
-            v-model="text"
+            type="text"
+            v-model="author.authorfname"
             placeholder="Author first name"
           ></b-form-input>
           <b-form-input
-            v-model="text"
+            v-model="author.authorlname"
             placeholder="Author last name"
           ></b-form-input>
-          <b-form-input v-model="text" placeholder="Author ID"></b-form-input>
-          <b-button variant="primary" type="submit" value="create author"
+          <b-form-input
+            v-model="author.authorid"
+            placeholder="Author ID"
+          ></b-form-input>
+          <b-button
+            variant="primary"
+            type="submit"
+            value="create author"
+            @click="submitAuthor"
             >Create Author</b-button
           >
         </b-col>
       </b-row>
     </div>
+
     <div id="genre-form">
       <b-row class="my-5">
         <b-col sm="6">
-          <b-form-input v-model="text" placeholder="Genre"></b-form-input>
-          <b-form-input v-model="text" placeholder="Genre ID"></b-form-input>
+          <b-form-input
+            v-model="genre.genrename"
+            placeholder="Genre"
+          ></b-form-input>
+          <b-form-input
+            v-model="genre.id"
+            placeholder="Genre ID"
+          ></b-form-input>
 
-          <b-button variant="primary" type="submit" value="create genre"
+          <b-button
+            variant="primary"
+            type="submit"
+            value="create genre"
+            @click="submitGenre"
             >Create Genre</b-button
           >
         </b-col>
@@ -33,22 +52,18 @@
     <div id="book-creator">
       <b-row class="my-5">
         <b-col sm="6">
-          <b-form-input v-model="text" placeholder="Book title"></b-form-input>
-          <b-form-input
-            v-model="text"
-            placeholder="Release year"
-          ></b-form-input>
+          <b-form-input placeholder="Book title"></b-form-input>
+          <b-form-input placeholder="Release year"></b-form-input>
+
           <b-form-select
-            v-model="select.selected"
             :options="select.optionOne"
             v-for="option in select.optionOne"
-            :key="option"
+            :key="option.text"
           ></b-form-select>
           <b-form-select
-            v-model="select.selected"
             :options="select.optionTwo"
             v-for="option in select.optionTwo"
-            :key="option"
+            :key="option.text"
           ></b-form-select>
           <b-button variant="success" type="submit" value="create author"
             >Create Book</b-button
@@ -65,20 +80,30 @@ export default {
     return {
       select: {
         selected: null,
-        optionOne: [{ value: null, text: "Author" }],
+        optionOne: [{ value: null, text: ["Author"] }],
         optionTwo: [{ value: null, text: "Genre" }],
       },
-      authors: [{ d }, {}, {}],
+      author: {
+        authorfname: "",
+        authorlname: "",
+        authorid: "",
+      },
+      genre: {
+        genrename: "",
+        genreid: "",
+      },
+      authors: [],
       genres: [],
       booklist: [],
-      AuthorObject: {},
-      GenreObject: {},
-      BookObject: {},
     };
   },
   methods: {
-    test: function (event) {
-      console.log(event.key);
+    submitAuthor: function () {
+      this.authors.push(this.author);
+    },
+    submitGenre: function () {
+      this.genres.push(this.genre);
+      console.log(this.genres);
     },
   },
 };
